99ff6183efe3ab23792a16d8f680bd6eaf018d85
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-26 06:44:07 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,55 @@
+#################################################
+#    _____ ____        _   
+#   / ____|  _ \      | |                  
+#  | |    | |_) | ___ | |_ 
+#  | |    |  _ < / _ \| __|
+#  | |____| |_) | (_) | |_ 
+#   \_____|____/ \___/ \__|
+#
+#################################################
+#
+# Java release. v0.1
+#################################################
+
+### Start of configuration ###
+
+###
+# Bot's nick.
+##
+var BotNick = "CBot";
+
+###
+# Bot's alter nick.
+##
+var BotAlt = "CBot-";
+
+###
+# Bot's ident.
+##
+var BotIdent = "CBot";
+
+###
+# Bot's realname.
+##
+var BotRealname = "CBot";
+
+### 
+# Bot's server.
+##
+var BotServer = "irc.address.org";
+
+###
+# Bot's port.
+##
+var BotPort = "6667";
+
+###
+# Bot's home channel.
+##
+var BotHome = "#CBot";
+
+# Bot's Admin's nick.
+##
+var BotAdmin = "Your-nick";
+
+### End of configuration ###
